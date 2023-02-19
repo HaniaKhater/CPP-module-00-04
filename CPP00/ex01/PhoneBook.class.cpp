6f8ed6929a502c8e69822231c6d1110caf122b8c
Hania hesham KHATER <hkhater@e1r12p21.clusters.42paris.fr>
2023-02-19 20:59:27 +0100

    jfhdgkkdf

--- CPP00/ex01/PhoneBook.class.cpp
+++ CPP00/ex01/PhoneBook.class.cpp
@@ -1,7 +1,3 @@
-
-//adjust layout of messages
-//setw for contact
-//only write blabla is invalid if not empty
 //fix table division
 //use is numeric in index check?
 
@@ -11,12 +7,6 @@
 PhoneBook::PhoneBook() { this->_curr_index = 0; }
 
 PhoneBook::~PhoneBook() {}
-
-static bool  is_numeric(std::string s) {
-    for (int i = 0; s[i], i++;)
-        if (!isdigit(s[i])) return false;
-    return true;
-}
 
 static int  set_feild(std::string &input) {
     while (1) {
@@ -57,44 +47,18 @@ int PhoneBook::set_contact() {
 }
 
 static std::string truncate(std::string s) {
-    if (s.length() > 10) return s.substr(0, 9) + ".";
+    if (s.length() > 10) 
+		return s.substr(0, 9) + ".";
     return (s);
 }
 
-//add the std::right before setw
-static void print_row(Contact contact, int index) {
-/*   if (index == 0) {
-        std::cout << std::right << std::setw(10) << "Index" << "|";
-        std::cout << std::right << std::setw(10) << "First Name" << "|";
-        std::cout << std::right << std::setw(10) << "Last Name" << "|";
-        std::cout << std::right << std::setw(10) << "Nickname" << std::endl;
-    }
-*/  std::cout << std::right << std::setw(10) << index << "|";
-    std::cout << std::right << std::setw(10) << truncate(contact.get_firstName()) << "|";
-    std::cout << std::right << std::setw(10) << truncate(contact.get_lastName()) << "|";
-    std::cout << std::right << std::setw(10) << truncate(contact.get_nickname()) << "|";
-    std::cout << std::right << std::setw(10) << truncate(contact.get_number()) << std::endl;
-}
-
-bool PhoneBook::_print_table(void) const {
-    if (this->_contacts[0].get_firstName().empty()) {
-        std::cout << "You have no contacts yet, please add contacts first." << std::endl;
-        return true;
-    }
-    for (int i = 0; i < 8, i++;) {
-        if (!this->_contacts[i].get_firstName().empty())
-            print_row(this->_contacts[i], i);
-    }
-    return false;
-}
-
 void PhoneBook::_display_info(int index) const {
-    std::cout << "-------------------------" << std::endl << "          CONTACT " << std::endl << "-------------------------" << std::endl;
-    std::cout << "First Name: " << this->_contacts[index].get_firstName() << std::endl;
-    std::cout << "Last Name: " << this->_contacts[index].get_lastName() << std::endl;
-    std::cout << "Nickname: " << this->_contacts[index].get_nickname() << std::endl;
-    std::cout << "Number: " << this->_contacts[index].get_number() << std::endl;
-    std::cout << "Darkest Secret: " << this->_contacts[index].get_secret() << std::endl;
+    std::cout << "----------------------------" << std::endl << "          CONTACT " << std::endl << "----------------------------" << std::endl;
+    std::cout << "First Name:      " << this->_contacts[index].get_firstName() << std::endl;
+    std::cout << "Last Name:       " << this->_contacts[index].get_lastName() << std::endl;
+    std::cout << "Nickname:        " << this->_contacts[index].get_nickname() << std::endl;
+    std::cout << "Number:          " << this->_contacts[index].get_number() << std::endl;
+    std::cout << "Darkest Secret:  " << this->_contacts[index].get_secret() << std::endl;
 }
 
 int PhoneBook::get_contact(void) const {
@@ -103,8 +67,20 @@ int PhoneBook::get_contact(void) const {
     bool        empty;
 
 
-    empty = _print_table();
-    if (empty) return EXIT_SUCCESS;
+    if (this->_contacts[0].get_firstName().empty()) {
+        std::cout << "You have no contacts yet, please add contacts first." << std::endl;
+        return true;
+    }
+	std::cout << std::right << std::setw(10) << "Index" << "|";
+    std::cout << std::right << std::setw(10) << "First Name" << "|";
+    std::cout << std::right << std::setw(10) << "Last Name" << "|";
+    std::cout << std::right << std::setw(10) << "Nickname" << std::endl;
+    for (int i = 0; _contacts[i].get_firstName().empty() == false; i++) {
+		std::cout << std::right << std::setw(10) << index << "|";
+    	std::cout << std::right << std::setw(10) << truncate(_contacts[i].get_firstName()) << "|";
+    	std::cout << std::right << std::setw(10) << truncate(_contacts[i].get_lastName()) << "|";
+    	std::cout << std::right << std::setw(10) << truncate(_contacts[i].get_nickname()) << std::endl;
+    }
     std::cout << "Enter the index of the contact you would like to view." << std::endl;
     while (true)
     {
@@ -119,6 +95,6 @@ int PhoneBook::get_contact(void) const {
                 break;
         }
     }
-    _display_info(index);
+    PhoneBook::_display_info(index);
     return EXIT_SUCCESS;
 }
